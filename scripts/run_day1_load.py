17602e9b51b53f5241b5f8b3c38e8609599aaf0d
MonaAlkhaldi <monaalkhaldi2002@gmail.com>
2025-12-22 20:37:40 +0300

    Add day 1 load script

---
+++ scripts/run_day1_load.py
@@ -0,0 +1,19 @@
+from pathlib import Path 
+from bootcamp_data.config import make_paths  
+from bootcamp_data.io import read_users_csv ,read_orders_csv , write_parquet , read_parquet
+from bootcamp_data.transforms import  enforce_schema
+
+
+#First we need tp find the file root 
+#resolve --- > give us the absoulote path
+#parent --> retrun the parent of the file and we said 1 becouse in my structure [0] --script [1] ---root [week2-data-work]
+ROOT=Path(__file__).resolve().parents[1]
+paths=make_paths(ROOT)
+
+order_df=read_orders_csv(paths.raw / "orders.csv")
+user_df=read_users_csv(paths.raw / "users.csv")
+order_df_enforce= enforce_schema(order_df)
+write_parquet(order_df_enforce ,paths.processed / "orders.parquet")
+
+
+
